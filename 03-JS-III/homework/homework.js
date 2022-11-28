d547d8df7e5e810ddd50fae6ebb21e6ad5acefca
PatryEli <patycita2908@gmail.com>
2022-11-28 07:58:29 -0300

    JSIII terminada

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -6,31 +6,50 @@
 
 function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
-    // Pista: usa un acumulador
+    // Pista: usa un controlador
     // Tu código:
+    
+    var suma=0;
+    for(var i = 0; i <=10; i++){
+      suma= suma + i;
+    } 
+    return suma
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var pares=[];
+  for (var i =0; i <= array.length; i ++){
+    if (array[i] % 2 ===0){
+      pares.push(array[i])
+    }
+  }
+  return pares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map((elemento)=> elemento**2)
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var suma= array.reduce ((acc,elemento) =>acc + elemento);
+  return suma;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var numeroenString = num.toString();
+  var longitud = numeroenString.length;
+  return longitud; 
 }
   
   
